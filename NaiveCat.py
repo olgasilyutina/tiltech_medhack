7bd4fb029ee09fcc59cd916bef7d363856a872a6
I <ibelyalov@yandex.ru>
2017-11-19 15:37:32 +0300

    Fix typo

--- NaiveCat.py
+++ NaiveCat.py
@@ -3,7 +3,8 @@ from sklearn.externals import joblib
 pipeline = joblib.load("NaiveMedCat.pkl")
 
 def prediction(example):
-    """Returns class with max probability""""
+    """Returns class with max probability"""
+    
     pred = pipeline.predict_proba([example])
     labels = ['medical_statements', 'medical_tests', 'operation_protocol', 'research_method']
     max_pred = (labels[np.argmax(pred)], np.max(pred))
