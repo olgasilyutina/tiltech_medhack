fc9120971f8cf73f2bc1c1bffc229e4a8f6f569b
olika <oyasilyutina@gmail.com>
2017-11-19 14:34:07 +0000

    Get 5th category

--- NaiveCat.py
+++ NaiveCat.py
@@ -9,5 +9,10 @@ def prediction(example):
     date = guess_date(example)
     pred = pipeline.predict_proba([example])
     labels = ['medical_statements', 'medical_tests', 'operation_protocol', 'research_method']
-    res = (labels[np.argmax(pred)], np.max(pred), date)
+    npm = np.max(pred)
+    if npm <= 0.7:
+        res = ("other", 0, date)
+    else:
+        res = (labels[np.argmax(pred)], npm, date)
+        
     return res
