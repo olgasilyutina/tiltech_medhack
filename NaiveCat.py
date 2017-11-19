701060ed2c8c94fa45c675fa459b826bd9e9cf09
olika <oyasilyutina@gmail.com>
2017-11-19 10:38:04 +0000

    Initial model

---
+++ NaiveCat.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.externals import joblib
+pipeline = joblib.load("NaiveMedCat.pkl")
+
+def prediction(example):
+    """Returns class with max probability""""
+    pred = pipeline.predict_proba([example])
+    labels = ['medical_statements', 'medical_tests', 'operation_protocol', 'research_method']
+    max_pred = (labels[np.argmax(pred)], np.max(pred))
+    return max_pred
